72b12dfa1cdabc09160ffe458a9c5d862e9deca6
North <robyter@gmail.com>
2026-07-12 16:59:59 +0200

    added commented out debug code

--- FixesPlugin.cs
+++ FixesPlugin.cs
@@ -3,10 +3,12 @@ using Gameplay.Battle.API.Signals;
 using Gameplay.Battle.API.Statuses;
 using Gameplay.Battle.API.Statuses.BattleEvents;
 using Gameplay.Battle.Implementation.BattleEvents.Effects;
+using Gameplay.Core.API;
 using HarmonyLib;
 using MelonLoader;
 using System.Reflection;
 using System.Reflection.Emit;
+using UnityEngine;
 
 [assembly: MelonInfo(typeof(IgniteTheForgeFixes.FixesPlugin), "Fixes", "1.0.0", "North")]
 [assembly: MelonGame("Floodgate Crew", "Blacksmith Ignite the Forge")]
@@ -14,6 +16,29 @@ using System.Reflection.Emit;
 namespace IgniteTheForgeFixes {
     public class FixesPlugin : MelonMod {
         internal static MelonLogger.Instance Log => Melon<FixesPlugin>.Logger;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
+            MelonLogger.Msg("StatChangeWeaponEnchantment");
+            var enchantments = Resources.FindObjectsOfTypeAll<StatChangeWeaponEnchantment>();
+            foreach (var enchantment in enchantments) {
+                MelonLogger.Msg($"{enchantment.name} {enchantment.Description}");
+            }
+
+            //MelonLogger.Msg("OreType");
+            //var oreTypes = Resources.FindObjectsOfTypeAll<OreType>();
+            //foreach (var oreType in oreTypes) {
+            //    MelonLogger.Msg($"{oreType.name} {oreType.OreColor}");
+            //    foreach (var enchantment in oreType.Enchantments) {
+            //        MelonLogger.Msg($"{enchantment.name} {enchantment.Description}");
+            //    }
+            //}
+
+            MelonLogger.Msg("GameConfig");
+            var gameConfigs = Resources.FindObjectsOfTypeAll<GameConfig>();
+            foreach (var gameConfig in gameConfigs) {
+                MelonLogger.Msg($"BaseCriticalMultiplier {gameConfig.BattleConfig.BaseCriticalMultiplier}");
+            }
+        }
     }
 
     [HarmonyPatch(typeof(KillTargetOnHitEffect), "EffectOnTarget")]
@@ -217,4 +242,147 @@ namespace IgniteTheForgeFixes {
         }
     }
 
+    //[HarmonyPatch(typeof(ChangeCritChanceForEnemiesAboveThresholdEffect), "OnUnitTurnStarted")]
+    //public class ChangeCritChanceForEnemiesAboveThresholdEffect_Patch {
+    //    [HarmonyPrefix]
+    //    public static bool Prefix(ChangeCritChanceForEnemiesAboveThresholdEffect __instance, UnitTurnStartedSignal signal) {
+    //        Traverse traverse = Traverse.Create(__instance);
+    //        var _hpPercentageThreshold = traverse.Field("_hpPercentageThreshold").GetValue<float>();
+    //        var battleMinigameController = traverse.Property("BattleMinigameController").GetValue<Gameplay.Battle.Implementation.BattleMinigameController>();
+    //        foreach (Unit unit in battleMinigameController.GetEnemyTeam().Units) {
+    //            FixesPlugin.Log.Msg($"unit.CurrentHealth {unit.CurrentHealth}");
+    //        }
+    //        FixesPlugin.Log.Msg($"_hpPercentageThreshold {_hpPercentageThreshold}");
+
+    //        return true; // execute original code
+    //    }
+    //}
+
+    //[HarmonyPatch(typeof(ApplyStatusToTargetOnSuccessfulAttackEffect), "EffectOnTarget")]
+    //public class ApplyStatusToTargetOnSuccessfulAttackEffect_Patch {
+    //    [HarmonyPostfix]
+    //    public static void Postfix(ApplyStatusToTargetOnSuccessfulAttackEffect __instance, Unit target) {
+    //        Traverse traverse = Traverse.Create(__instance);
+    //        Unit _owner = traverse.Field("_owner").GetValue<Unit>();
+    //        FixesPlugin.Log.Msg($"{_owner.Name} crit {_owner.CritDamageModifier}");
+    //    }
+    //}
+
+    /*
+    [HarmonyPatch(typeof(KillTargetOnHitEffect), "EffectOnTarget")]
+    public class KillTargetOnHitEffect_Patch {
+        [HarmonyPrefix]
+        public static bool Prefix(KillTargetOnHitEffect __instance, Unit target) {
+            try {
+                Traverse traverse = Traverse.Create(__instance);
+                float healthThreshold = traverse.Field("_healthThreshold").GetValue<float>();
+                float bonusHealthThresholdPerStatus = traverse.Field("_bonusHealthThresholdPerStatus").GetValue<float>();
+                BattleEventConfig bonusStatus = traverse.Field("_bonusStatus").GetValue<BattleEventConfig>();
+
+                float computedThreashold = healthThreshold;
+                computedThreashold += target.Statuses
+                    .OfType<BattleEvent>()
+                    .Where(it => it.Config == bonusStatus)
+                    .Sum(it => bonusHealthThresholdPerStatus * it.Stacks);
+
+                if (target.CurrentHealthPercent <= computedThreashold) {
+                    target.Kill();
+                }
+
+                return false; // skip original code
+            } catch (Exception ex) {
+                FixesPlugin.Log.Error($"Failed fixing KillTargetOnHitEffect.EffectOnTarget: {ex}");
+                return true; // execute original code
+            }
+        }
+    }
+    */
+
+    //[HarmonyPatch(typeof(Gameplay.AlloyMelting.Impl.CrucibleBehaviour), "CheckInsertedOre")]
+    //public class CrucibleBehaviour_Patch {
+    //    [HarmonyPostfix]
+    //    public static void Postfix(Gameplay.AlloyMelting.Impl.CrucibleBehaviour __instance, Gameplay.AlloyMelting.Impl.CrucibleOreReceiver updatedReceiver) {
+    //        if (__instance == null) return;
+    //        try {
+    //            FixesPlugin.Log.Msg(">>>>>>>>>>>>>>>>>>>>>>>>");
+    //            var gameConfig = Traverse.Create(__instance).Property("GameConfig").GetValue<Gameplay.Core.API.GameConfig>();
+
+    //            foreach (var followerPrototype in gameConfig.FollowersConfig.FollowerPrototypes) {
+    //                FixesPlugin.Log.Msg($"## {followerPrototype.name}");
+    //                FixesPlugin.Log.Msg($"Health: {followerPrototype.MaxHealthRange}");
+    //                FixesPlugin.Log.Msg($"Damage: {followerPrototype.DamageModRange}");
+    //                FixesPlugin.Log.Msg($"Speed: {followerPrototype.SpeedModRange}");
+    //                FixesPlugin.Log.Msg($"CritChance: {followerPrototype.CritChangeRange}");
+    //                FixesPlugin.Log.Msg($"StartingArmor: {followerPrototype.StartingArmorRange.x}");
+    //                FixesPlugin.Log.Msg($"ChargesToUse: {followerPrototype.TalentTreeConfig.ActiveAbilityConfig.ChargesToUse}");
+    //                FixesPlugin.Log.Msg($"ChargesGainedOnBeingAttacked: {followerPrototype.TalentTreeConfig.ActiveAbilityConfig.ChargesGainedOnBeingAttacked}");
+    //                FixesPlugin.Log.Msg($"ChargesGainedOnPerformingAttack: {followerPrototype.TalentTreeConfig.ActiveAbilityConfig.ChargesGainedOnPerformingAttack}");
+    //                FixesPlugin.Log.Msg($"ChargesGainedOnPerformingActiveAbility: {followerPrototype.TalentTreeConfig.ActiveAbilityConfig.ChargesGainedOnPerformingActiveAbility}");
+    //                FixesPlugin.Log.Msg($"ActiveAbility: {followerPrototype.TalentTreeConfig.ActiveAbilityConfig.Description} {string.Join(", ", followerPrototype.TalentTreeConfig.ActiveAbilityConfig.EffectsTriggeredOnAbilityActivation.Select(e => e.EffectType.ToString() + " " + string.Join(", ", e.Triggers)))}");
+    //                var abilityNodes = followerPrototype.TalentTreeConfig.TalentTreeGraph.Nodes().OfType<Gameplay.Followers.API.AbilityNode>();
+    //                bool first = true;
+    //                foreach (var abilityNode in abilityNodes) {
+    //                    if (abilityNode.BattleEventConfig.Description != null && abilityNode.BattleEventConfig.Description.Length > 0) {
+    //                        var prefix = "*";
+    //                        if (first) {
+    //                            first = false;
+    //                            if (abilityNodes.Count(it => it.BattleEventConfig.Description != null && it.BattleEventConfig.Description.Length > 0) == 4) {
+    //                                prefix = "Passive:";
+    //                            }
+    //                        }
+    //                        FixesPlugin.Log.Msg($"{prefix} {abilityNode.BattleEventConfig.Description} {string.Join(", ", abilityNode.BattleEventConfig.Effects.Select(e => e.EffectType + " " + string.Join(", ", e.Triggers)))}");
+    //                    }
+    //                }
+    //                FixesPlugin.Log.Msg("");
+    //            }
+
+    //        } catch (Exception ex) {
+    //            FixesPlugin.Log.Error($"Failed fixing CrucibleBehaviour.CheckInsertedOre: {ex}");
+    //        }
+    //    }
+    //}
+
+    //[HarmonyPatch(typeof(Gameplay.RunesEmbedding.Impl.RuneTableRuneReceiver), "ReceiveItem")]
+    //public class RuneTableRuneReceiver_Patch {
+    //    [HarmonyPostfix]
+    //    public static void Postfix(Gameplay.RunesEmbedding.Impl.RuneTableRuneReceiver __instance, Gameplay.Core.API.DraggableItem<Gameplay.Core.API.RuneItem> draggableItem) {
+    //        if (__instance == null) return;
+    //        try {
+    //            FixesPlugin.Log.Msg($"{draggableItem.Item.RuneType.Name.GetLocalizedString()}");
+    //            foreach (var enchantment in draggableItem.Item.RuneType.WeaponEnchantment) {
+    //                try {
+    //                    FixesPlugin.Log.Msg($">>> {enchantment.EnchantmentName.GetLocalizedString()}");
+    //                    foreach (var effect in enchantment.Effects) {
+    //                        FixesPlugin.Log.Msg($">>>>>> {effect.EffectType} {effect.Target} {string.Join(", ", effect.Triggers)}");
+    //                        //if (effect is HealingWaterEventEffectConfig healingWaterEffectConfig) {
+    //                        //    FixesPlugin.Log.Msg($">>>>>> HealAmount: {healingWaterEffectConfig.HealAmount}");
+    //                        //    FixesPlugin.Log.Msg($">>>>>> RandomChance: {healingWaterEffectConfig.RandomChance}");
+    //                        //    FixesPlugin.Log.Msg($">>>>>> BonusChancePerStatusTime: {healingWaterEffectConfig.BonusChancePerStatusTime}");
+    //                        //    FixesPlugin.Log.Msg($">>>>>> TargetStatus: {healingWaterEffectConfig.TargetStatus}");
+    //                        //}
+    //                    }
+    //                } catch (Exception) {
+    //                }
+    //            }
+
+    //        } catch (Exception ex) {
+    //            FixesPlugin.Log.Error($"Failed fixing RuneTableRuneReceiver.CheckInsertedOre: {ex}");
+    //        }
+    //    }
+    //}
+
+
+    //[HarmonyPatch(typeof(AttackAllOnCritEffect), "EffectOnTarget")]
+    //public static class AttackAllOnCritEffectPatch {
+    //    [HarmonyTranspiler]
+    //    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+    //        List<CodeInstruction> codes = new(instructions);
+
+    //        for (int i = 0; i < codes.Count - 1; i++) {
+    //            FixesPlugin.Log.Msg(codes[i]);
+    //        }
+
+    //        return codes;
+    //    }
+    //}
 }
